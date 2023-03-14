9edd6201219e657f2f0a1a0153073851488f998e
namedmichi <j.technology@web.de>
2023-03-14 13:44:39 +0100

    push

--- ecommerce-product-page-main/scripts.js
+++ ecommerce-product-page-main/scripts.js
@@ -1,3 +1,6 @@
+var number = 0
+
+
 function clickLess(){
     var count = parseInt(document.getElementById("counter").innerHTML);
     if (count > 0){
@@ -33,4 +36,83 @@ function addCart(){
 function cartRemove(){
     document.getElementById("empty").style.display = "block"
     document.getElementById("full").style.display = "none"
+}
+
+
+function clickPic1(){
+    document.getElementById("product-img").style.background = "url('images/image-product-1.jpg') no-repeat no-repeat"
+    document.getElementById("product-img").style.backgroundSize = "400px"
+}
+
+function clickPic2(){
+    document.getElementById("product-img").style.background = "url('images/image-product-2.jpg') no-repeat no-repeat"
+    document.getElementById("product-img").style.backgroundSize = "400px"
+}
+
+function clickPic3(){
+    document.getElementById("product-img").style.background = "url('images/image-product-3.jpg') no-repeat no-repeat"
+    document.getElementById("product-img").style.backgroundSize = "400px"
+}
+
+function clickPic4(){
+    document.getElementById("product-img").style.background = "url('images/image-product-4.jpg') no-repeat no-repeat"
+    document.getElementById("product-img").style.backgroundSize = "400px"
+}
+function openNav() {
+    document.getElementById("nav-overlay").style.display = "block"
+    document.getElementById("nav-overlay").style.width = "100%";
+    document.body.classList.add("overlay-active");
+  }
+  
+function closeNav() {
+    document.getElementById("nav-overlay").style.display = "none";
+    document.getElementById("nav-overlay").style.width = "0%";
+    document.body.classList.remove("overlay-active");
+  }
+
+function nextPic(){
+    if (number < 3){
+        number += 1;
+        switch (number) {
+            case 1:
+                
+                document.getElementById("product-img").style.background = "url('images/image-product-2.jpg') no-repeat no-repeat"
+                document.getElementById("product-img").style.backgroundSize = "370px"
+                break;
+            case 2:
+                document.getElementById("product-img").style.background = "url('images/image-product-3.jpg') no-repeat no-repeat"
+                document.getElementById("product-img").style.backgroundSize = "370px"
+                break;
+            case 3:
+                document.getElementById("product-img").style.background = "url('images/image-product-4.jpg') no-repeat no-repeat"
+                document.getElementById("product-img").style.backgroundSize = "370px"
+                break;
+            
+            default:
+                break;
+        }
+    }
+}
+function prevPic(){
+    if (number > 0){
+        number -= 1;
+        switch (number) {
+            case 1:
+                
+                document.getElementById("product-img").style.background = "url('images/image-product-2.jpg') no-repeat no-repeat"
+                document.getElementById("product-img").style.backgroundSize = "370px"
+                break;
+            case 2:
+                document.getElementById("product-img").style.background = "url('images/image-product-3.jpg') no-repeat no-repeat"
+                document.getElementById("product-img").style.backgroundSize = "370px"
+                break;
+            case 0:
+                document.getElementById("product-img").style.background = "url('images/image-product-1.jpg') no-repeat no-repeat"
+                document.getElementById("product-img").style.backgroundSize = "370px"
+                break;
+            
+            default:
+                break;
+        }
+    }
 }
