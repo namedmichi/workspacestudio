d0322f132a9a03dc4b5c14202a71d6a0c115d66e
namedmichi <j.technology@web.de>
2023-03-26 16:53:27 +0200

    push

--- angular-test/firstTest/src/app/pages/nav-bar/nav-bar.component.ts
+++ angular-test/firstTest/src/app/pages/nav-bar/nav-bar.component.ts
@@ -5,6 +5,88 @@ import { Component } from '@angular/core';
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
+
 export class NavBarComponent {
+  isDark$: boolean = true;
+  
+  
+  
+  setDark(){
+
+    if (this.isDark$) {
+      
+      document.body.style.backgroundColor = "var(--Very-Light-Gray)"
+      
+      var ps = document.getElementsByTagName("p")
+      var navs = document.getElementsByTagName("nav")
+      var h2s = document.getElementsByTagName("h2")
+      var h3s = document.getElementsByTagName("h3")
+      var cards = document.getElementsByName("country-cardes")
+      for (let index = 0; index < ps.length; index++) {
+        const element = ps[index];
+        element.style.color = "black"
+        
+      }
+      for (let index = 0; index < h2s.length; index++) {
+        const element = h2s[index];
+        element.style.color = "black"
+        
+      }
+      for (let index = 0; index < h3s.length; index++) {
+        const element = h3s[index];
+        element.style.color = "black"
+        
+      }
+      for (let index = 0; index < cards.length; index++) {
+        const element = cards[index]
+        element.style.backgroundColor = "white";
+        
+        
+      }
+      for (let index = 0; index < navs.length; index++) {
+        const element = navs[index]
+        element.style.backgroundColor = "white";
+        
+        
+      }
+      this.isDark$ = false;
+    }else{
+      document.body.style.backgroundColor = "var(--Very-Dark-Blue2)"
+      
+      var ps = document.getElementsByTagName("p")
+      var navs = document.getElementsByTagName("nav")
+      var h2s = document.getElementsByTagName("h2")
+      var h3s = document.getElementsByTagName("h3")
+      var cards = document.getElementsByName("country-cardes")
+      for (let index = 0; index < ps.length; index++) {
+        const element = ps[index];
+        element.style.color = "white"
+        
+      }
+      for (let index = 0; index < h2s.length; index++) {
+        const element = h2s[index];
+        element.style.color = "white"
+        
+      }
+      for (let index = 0; index < h3s.length; index++) {
+        const element = h3s[index];
+        element.style.color = "white"
+        
+      }
+      for (let index = 0; index < cards.length; index++) {
+        const element = cards[index]
+        element.style.backgroundColor = "var(--Very-Dark-Blue)";
+        
+        
+      }
+      for (let index = 0; index < navs.length; index++) {
+        const element = navs[index]
+        element.style.backgroundColor = "var(--Dark-Blue)";
+        
+        
+      }
+      this.isDark$ = true;
+    }
+  }
 
 }
