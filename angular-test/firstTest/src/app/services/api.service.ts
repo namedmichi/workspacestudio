5d22ff6c5b5809bffe47a0d54de993d1d014f5bf
namedmichi <j.technology@web.de>
2023-03-26 21:05:40 +0200

    push

--- angular-test/firstTest/src/app/services/api.service.ts
+++ angular-test/firstTest/src/app/services/api.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Country } from '../types/api';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
+  borderCountries$: Observable<Country[]>;
   private api = 'https://restcountries.com/v3.1';
 
   constructor(private http: HttpClient) {}
@@ -14,12 +16,18 @@ export class ApiService {
   getAllCountries() {
     return this.http.get<Country[]>(`https://restcountries.com/v2/all`);
   }
-
+  
   getCountryByName(name: string) {
     return this.http
-      .get<Country[]>(`${this.api}/name/${name}`)
-      .pipe(map(([res]) => res));
+    .get<Country[]>(`${this.api}/name/${name}`)
+    .pipe(map(([res]) => res));
   }
+  getCountriesByCodes(codes: string[]) {
+    console.log(`https://restcountries.com/v3.1/alpha?codes=${codes.join(',')}`);
 
+    return this.http.get<Country[]>(
+      `${this.api}/alpha?codes=${codes.join(',')}`
+    );
+  }
 
 }
