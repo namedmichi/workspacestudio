b4c3c318f0047d5d8c32d78151477c5c7aa726f8
namedmichi <j.technology@web.de>
2023-05-15 11:48:00 +0200

    push

---
+++ counterScript.js
@@ -0,0 +1,64 @@
+var counterText;
+var html;
+var css;
+var js;
+var ts;
+var htmlChars;
+var cssChars;
+var jsChars;
+var tsChars;
+var htmlWords;
+var cssWords;
+var tsWords;
+var jsWords;
+var htmlLines;
+var cssLines;
+var tsLines;
+
+var jsLines;
+fetch('counterFile.txt')
+	.then((response) => response.text())
+	.then((text) => (counterText = text))
+	.then((text) => {
+		counterText = counterText.split(';');
+		console.log(counterText);
+		console.log(typeof counterText);
+		var lenght = counterText.length;
+		html = counterText[lenght - 5];
+		css = counterText[lenght - 4];
+		js = counterText[lenght - 3];
+		ts = counterText[lenght - 2];
+
+		console.log(html);
+		html = html.split(' ');
+		css = css.split(' ');
+		js = js.split(' ');
+		ts = ts.split(' ');
+		console.log(css[0].substring(1));
+		console.log(js);
+		htmlChars = html[0];
+		htmlWords = html[3];
+		htmlLines = html[6];
+		cssChars = css[0].substring(1);
+		cssWords = css[3];
+		cssLines = css[6];
+		jsChars = js[0].substring(1);
+		jsWords = js[3];
+		jsLines = js[6];
+		tsChars = ts[0].substring(1);
+		tsChars = ts[0];
+		tsWords = ts[3];
+		tsLines = ts[6];
+		document.getElementById('htmlChars').innerHTML += htmlChars;
+		document.getElementById('htmlWords').innerHTML += htmlWords;
+		document.getElementById('htmlLines').innerHTML += htmlLines;
+		document.getElementById('cssChars').innerHTML += cssChars;
+		document.getElementById('cssWords').innerHTML += cssWords;
+		document.getElementById('cssLines').innerHTML += cssLines;
+		document.getElementById('jsChars').innerHTML += jsChars;
+		document.getElementById('jsWords').innerHTML += jsWords;
+		document.getElementById('jsLines').innerHTML += jsLines;
+		document.getElementById('tsChars').innerHTML += tsChars;
+		document.getElementById('tsWords').innerHTML += tsWords;
+		document.getElementById('tsLines').innerHTML += tsLines;
+	});
---
+++ getCounter.sh
@@ -0,0 +1,27 @@
+tslines=$(git ls-files | grep -E '\.ts |\.ts$'  |  wc -l)
+tsrightline=$(($tslines-3))
+jslines=$(git ls-files | grep -E '\.js |\.js$'  |  wc -l)
+jsrightline=$(($jslines-3))
+
+htmlChars=$(git ls-files | grep -E '\.html |\.html$'  | xargs wc -c | tail -1 | sed 's/$/ Chars \n/') 
+cssChars=$(git ls-files | grep -E '\.css |\.css$'  | xargs wc -c | tail -1 | sed 's/$/ Chars \n/') 
+jsChars=$(git ls-files | grep -E '\.js |\.js$' | tail -$jsrightline | xargs wc -c |tail -1 | sed 's/$/ Chars \n/') 
+tsChars=$(git ls-files | grep -E '\.ts |\.ts$' | tail -$tsrightline  | xargs wc -c | tail -1 | sed 's/$/ Chars \n/') 
+htmlWords=$(git ls-files | grep -E '\.html |\.html$'  | xargs wc -w | tail -1 | sed 's/$/ Words \n/') 
+cssWords=$(git ls-files | grep -E '\.css |\.css$'  | xargs wc -w | tail -1 | sed 's/$/ Words \n/') 
+jsWords=$(git ls-files | grep -E '\.js |\.js$' | tail -$jsrightline | xargs wc -w |tail -1 | sed 's/$/ Words \n/') 
+tsWords=$(git ls-files | grep -E '\.ts |\.ts$' | tail -$tsrightline | xargs wc -w | tail -1 | sed 's/$/ Words \n/') 
+htmlLines=$(git ls-files | grep -E '\.html |\.html$'  | xargs wc -l | tail -1 | sed 's/$/ Lines html \n/') 
+cssLines=$(git ls-files | grep -E '\.css |\.css$'  | xargs wc -l | tail -1 | sed 's/$/ Lines css \n/') 
+jsLines=$(git ls-files | grep -E '\.js |\.js$' | tail -$jsrightline | xargs wc -l |tail -1 | sed 's/$/ Lines javascript \n/') 
+tsLines=$(git ls-files | grep -E '\.ts |\.ts$' | tail -$tsrightline | xargs wc -l | tail -1 | sed 's/$/ Lines typescript \n/') 
+
+
+
+echo $htmlChars $htmlWords $htmlLines ";"  >> CounterFile.txt;
+echo $cssChars $cssWords $cssLines ";"  >> CounterFile.txt;
+echo $jsChars $jsWords $jsLines ";"  >> CounterFile.txt;
+echo $tsChars $tsWords $tsLines ";"  >> CounterFile.txt;
+echo "Am "$(date +"%c") >> CounterFile.txt;
+echo "----------------" >> CounterFile.txt;
+exit 0
